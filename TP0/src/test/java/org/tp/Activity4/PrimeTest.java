0431e7d133868ac64986428e86fcb52c97d97f56
Beghdad Amine <topplay020@gmail.com>
2024-02-19 10:29:42 +0100

    Activity4 add testPrimeNumbers and testNonPrimeNumbers

--- TP0/src/test/java/org/tp/Activity4/PrimeTest.java
+++ TP0/src/test/java/org/tp/Activity4/PrimeTest.java
@@ -1,4 +1,36 @@
 package org.tp.Activity4;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PrimeTest {
+
+    @Test
+    public void testPrimeNumbers() {
+        assertTrue(Prime.isPrime(2));
+        assertTrue(Prime.isPrime(3));
+        assertTrue(Prime.isPrime(5));
+        assertTrue(Prime.isPrime(7));
+        assertTrue(Prime.isPrime(11));
+        assertTrue(Prime.isPrime(13));
+        assertTrue(Prime.isPrime(17));
+        assertTrue(Prime.isPrime(19));
+        assertTrue(Prime.isPrime(23));
+        assertTrue(Prime.isPrime(29));
+    }
+
+    @Test
+    public void testNonPrimeNumbers() {
+        assertFalse(Prime.isPrime(0));
+        assertFalse(Prime.isPrime(1));
+        assertFalse(Prime.isPrime(4));
+        assertFalse(Prime.isPrime(6));
+        assertFalse(Prime.isPrime(8));
+        assertFalse(Prime.isPrime(9));
+        assertFalse(Prime.isPrime(10));
+        assertFalse(Prime.isPrime(12));
+        assertFalse(Prime.isPrime(14));
+        assertFalse(Prime.isPrime(15));
+    }
+
 }
