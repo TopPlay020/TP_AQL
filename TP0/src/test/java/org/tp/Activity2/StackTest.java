a37aade91670faf6db2f76da5511df3348a880e3
Beghdad Amine <topplay020@gmail.com>
2024-02-19 10:02:12 +0100

    Activity2 Test Complete

--- TP0/src/test/java/org/tp/Activity2/StackTest.java
+++ TP0/src/test/java/org/tp/Activity2/StackTest.java
@@ -1,4 +1,66 @@
 package org.tp.Activity2;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StackTest {
+
+    private Stack stack;
+
+    @BeforeEach
+    public void setUp() {
+        stack = new Stack();
+    }
+
+    @Test
+    public void testPushAndPop() {
+        stack.push(1);
+        stack.push(2);
+        assertEquals(2, stack.pop());
+        assertEquals(1, stack.pop());
+    }
+
+    @Test
+    public void testPeek() {
+        stack.push(1);
+        assertEquals(1, stack.peek());
+        assertEquals(1, stack.peek());
+        stack.push(2);
+        assertEquals(2, stack.peek());
+        assertEquals(2, stack.peek());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(stack.isEmpty());
+        stack.push(1);
+        assertFalse(stack.isEmpty());
+        stack.pop();
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(0, stack.size());
+        stack.push(1);
+        assertEquals(1, stack.size());
+        stack.push(2);
+        assertEquals(2, stack.size());
+        stack.pop();
+        assertEquals(1, stack.size());
+        stack.pop();
+        assertEquals(0, stack.size());
+    }
+
+    @Test
+    public void testPopEmptyStack() {
+        assertThrows(IllegalStateException.class, stack::pop);
+    }
+
+    @Test
+    public void testPeekEmptyStack() {
+        assertThrows(IllegalStateException.class, stack::peek);
+    }
 }
